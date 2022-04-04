ea8a85ca21e8f4e1f3df7e281dd10eeb667b05ea
Anurag Verma <new.anurag.verma@gmail.com>
2022-04-04 23:25:44 +0530

    Added kernel headers and kernel image for 4.14.271 version for x86

---
+++ linux_kernels/kernel_headers/x86/linux-headers-4.14.271/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#2 SMP Mon Apr 4 22:25:16 IST 2022"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "avlaptop"
+#define LINUX_COMPILER "gcc version 7.5.0 (Ubuntu 7.5.0-3ubuntu1~18.04)"
