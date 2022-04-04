8de114e78e8c6bc2c5995f1cffc2b42d15b00169
Anurag Verma <new.anurag.verma@gmail.com>
2022-04-04 16:45:27 +0530

    Added headers for linux-headers-3.10.1-avm.x86 for x86 architecture

---
+++ linux_kernels/kernel_headers/x86/linux-headers-3.10.1-avm.x86/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 4 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#4 SMP Mon Apr 4 16:05:31 IST 2022"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "avlaptop"
+#define LINUX_COMPILER "gcc version 4.8.5 (Ubuntu 4.8.5-4ubuntu8) "
---
+++ linux_kernels/kernel_headers/x86/linux-headers-3.10.1-avm.x86/scripts/mod/.devicetable-offsets.s.cmd
@@ -0,0 +1,53 @@
+cmd_scripts/mod/devicetable-offsets.s := /usr/bin/gcc-4.8 -Wp,-MD,scripts/mod/.devicetable-offsets.s.d  -nostdinc -isystem /usr/lib/gcc/x86_64-linux-gnu/4.8/include -nostdinc -isystem /usr/lib/gcc/x86_64-linux-gnu/4.8/include -I/home/averma/kernel/linux-3.10.1/linux-3.10.1/arch/x86/include -Iarch/x86/include/generated  -Iinclude -I/home/averma/kernel/linux-3.10.1/linux-3.10.1/arch/x86/include/uapi -Iarch/x86/include/generated/uapi -I/home/averma/kernel/linux-3.10.1/linux-3.10.1/include/uapi -Iinclude/generated/uapi -include /home/averma/kernel/linux-3.10.1/linux-3.10.1/include/linux/kconfig.h -D__KERNEL__ -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs -fno-strict-aliasing -fno-common -Werror-implicit-function-declaration -Wno-format-security -fno-delete-null-pointer-checks -O2 -m64 -mno-sse -mpreferred-stack-boundary=3 -mtune=generic -mno-red-zone -mcmodel=kernel -funit-at-a-time -maccumulate-outgoing-args -DCONFIG_X86_X32_ABI -DCONFIG_AS_CFI=1 -DCONFIG_AS_CFI_SIGNAL_FRAME=1 -DCONFIG_AS_CFI_SECTIONS=1 -DCONFIG_AS_FXSAVEQ=1 -DCONFIG_AS_AVX=1 -DCONFIG_AS_AVX2=1 -pipe -Wno-sign-compare -fno-asynchronous-unwind-tables -mno-sse -mno-mmx -mno-sse2 -mno-3dnow -mno-avx -Wframe-larger-than=1024 -fno-stack-protector -Wno-unused-but-set-variable -fno-omit-frame-pointer -fno-optimize-sibling-calls -g -pg -mfentry -DCC_USING_FENTRY -Wdeclaration-after-statement -Wno-pointer-sign -fno-strict-overflow -fconserve-stack -DCC_HAVE_ASM_GOTO -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs -fno-strict-aliasing -fno-common -Werror-implicit-function-declaration -Wno-format-security -fno-delete-null-pointer-checks -O2 -m64 -mno-sse -mpreferred-stack-boundary=3 -mtune=generic -mno-red-zone -mcmodel=kernel -funit-at-a-time -maccumulate-outgoing-args -DCONFIG_X86_X32_ABI -DCONFIG_AS_CFI=1 -DCONFIG_AS_CFI_SIGNAL_FRAME=1 -DCONFIG_AS_CFI_SECTIONS=1 -DCONFIG_AS_FXSAVEQ=1 -DCONFIG_AS_AVX=1 -DCONFIG_AS_AVX2=1 -pipe -Wno-sign-compare -fno-asynchronous-unwind-tables -mno-sse -mno-mmx -mno-sse2 -mno-3dnow -mno-avx -Wframe-larger-than=1024 -fno-stack-protector -Wno-unused-but-set-variable -fno-omit-frame-pointer -fno-optimize-sibling-calls -g -pg -mfentry -DCC_USING_FENTRY -Wdeclaration-after-statement -Wno-pointer-sign -fno-strict-overflow -fconserve-stack -DCC_HAVE_ASM_GOTO -fno-pic    -D"KBUILD_STR(s)=\#s" -D"KBUILD_BASENAME=KBUILD_STR(devicetable_offsets)"  -D"KBUILD_MODNAME=KBUILD_STR(devicetable_offsets)" -fverbose-asm -S -o scripts/mod/devicetable-offsets.s scripts/mod/devicetable-offsets.c
+
+source_scripts/mod/devicetable-offsets.s := scripts/mod/devicetable-offsets.c
+
+deps_scripts/mod/devicetable-offsets.s := \
+  include/linux/kbuild.h \
+  include/linux/mod_devicetable.h \
+  include/linux/types.h \
+    $(wildcard include/config/uid16.h) \
+    $(wildcard include/config/lbdaf.h) \
+    $(wildcard include/config/arch/dma/addr/t/64bit.h) \
+    $(wildcard include/config/phys/addr/t/64bit.h) \
+    $(wildcard include/config/64bit.h) \
+  include/uapi/linux/types.h \
+  /home/averma/kernel/linux-3.10.1/linux-3.10.1/arch/x86/include/uapi/asm/types.h \
+  /home/averma/kernel/linux-3.10.1/linux-3.10.1/include/uapi/asm-generic/types.h \
+  include/asm-generic/int-ll64.h \
+  include/uapi/asm-generic/int-ll64.h \
+  /home/averma/kernel/linux-3.10.1/linux-3.10.1/arch/x86/include/uapi/asm/bitsperlong.h \
+  include/asm-generic/bitsperlong.h \
+  include/uapi/asm-generic/bitsperlong.h \
+  /home/averma/kernel/linux-3.10.1/linux-3.10.1/include/uapi/linux/posix_types.h \
+  include/linux/stddef.h \
+  include/uapi/linux/stddef.h \
+  include/linux/compiler.h \
+    $(wildcard include/config/sparse/rcu/pointer.h) \
+    $(wildcard include/config/trace/branch/profiling.h) \
+    $(wildcard include/config/profile/all/branches.h) \
+    $(wildcard include/config/enable/must/check.h) \
+    $(wildcard include/config/enable/warn/deprecated.h) \
+    $(wildcard include/config/kprobes.h) \
+  include/linux/compiler-gcc.h \
+    $(wildcard include/config/arch/supports/optimized/inlining.h) \
+    $(wildcard include/config/optimize/inlining.h) \
+  include/linux/compiler-gcc4.h \
+    $(wildcard include/config/arch/use/builtin/bswap.h) \
+  /home/averma/kernel/linux-3.10.1/linux-3.10.1/arch/x86/include/asm/posix_types.h \
+    $(wildcard include/config/x86/32.h) \
+  /home/averma/kernel/linux-3.10.1/linux-3.10.1/arch/x86/include/uapi/asm/posix_types_64.h \
+  /home/averma/kernel/linux-3.10.1/linux-3.10.1/include/uapi/asm-generic/posix_types.h \
+  include/linux/uuid.h \
+  include/uapi/linux/uuid.h \
+  include/linux/string.h \
+    $(wildcard include/config/binary/printf.h) \
+  /usr/lib/gcc/x86_64-linux-gnu/4.8/include/stdarg.h \
+  include/uapi/linux/string.h \
+  /home/averma/kernel/linux-3.10.1/linux-3.10.1/arch/x86/include/asm/string.h \
+  /home/averma/kernel/linux-3.10.1/linux-3.10.1/arch/x86/include/asm/string_64.h \
+    $(wildcard include/config/kmemcheck.h) \
+
+scripts/mod/devicetable-offsets.s: $(deps_scripts/mod/devicetable-offsets.s)
+
+$(deps_scripts/mod/devicetable-offsets.s):
